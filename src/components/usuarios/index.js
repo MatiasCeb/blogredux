d1d62db6081fcd74f0270e2804d5feb065e7377f
matiasceb <matiasceb8812@gmail.com>
2021-10-08 20:37:53 -0300

    21-10-08

--- src/components/usuarios/index.js
+++ src/components/usuarios/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Spinner from "../general/Spinner";
 import * as usuariosActions from "../../actions/usuariosActions";
 
 class Usuarios extends Component {
 
   componentDidMount() {
     this.props.traerTodos();
+  }
+
+  ponerContenido = () => {
+    if(this.props.cargando) {
+      return <Spinner />;
+    }
+
+    if (this.)
+    return (
+      <table className="tabla">
+      <thead>
+        <tr>
+          <th>
+            Nombre
+          </th>
+          <th>
+            Correo
+          </th>
+          <th>
+            Enlace
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        { this.ponerFilas() }
+      </tbody>
+    </table>
+    )
   }
 
   ponerFilas = () => (
@@ -27,24 +56,7 @@ class Usuarios extends Component {
   render() {
     return (
       <div>
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>
-                Nombre
-              </th>
-              <th>
-                Correo
-              </th>
-              <th>
-                Enlace
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.ponerFilas() }
-          </tbody>
-        </table>
+        {this.ponerContenido()}
       </div>
     );
   }
